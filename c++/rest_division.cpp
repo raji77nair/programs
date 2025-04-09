eda2b244f9796a0167b2325e9d2c9d4d0caac87b
raji77nair <rajinair3445@gmail.com>
2025-04-09 21:50:13 +0530

    rest_division.cpp

---
+++ c++/rest_division.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x,y,i,num,result;
+cout<< "rest of division"<<endl;
+cin>>x;
+cin>>y;
+vector <int> result;
+for (int j = x; j <=y; j++)
+{
+    if(j%5==2||j%5==3){
+        result.push_back(i);
+    }
+}
+for(int i num:result){
+    cout<<num<<endl;
+}
+cout<<i<<endl;
+    return 0;
+}
