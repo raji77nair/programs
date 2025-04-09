eb8a4eca026627c7bbb9de17551dcb4a8a7a3c54
raji77nair <rajinair3445@gmail.com>
2025-04-09 19:30:41 +0530

    new password

---
+++ c++/fixed_password.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int password;
+cout<< "fixed password" <<endl;
+cin>> password;
+if(password !=2002){
+    cout<<"sneha invalida"<<endl;
+}
+else{
+    cout<<"acesso permitido"<<endl;
+}
+cout<< password <<endl;
+    return 0;
+}
