08676844ed1666f6c3937ff19943ebab5d69c3bd
raji77nair <rajinair3445@gmail.com>
2025-06-24 11:11:49 +0530

    Add files via upload

---
+++ c++/ascending.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+using namespace std;
+
+int main() {
+int m,n,first,last;
+cin>>m>>n;
+first=m;
+last=n;
+if(m>n){
+    cout<<"Decrescente"<<endl;
+}
+else{
+    cout<<"Crescente"<<endl;
+}
+
+return 0;
+}
